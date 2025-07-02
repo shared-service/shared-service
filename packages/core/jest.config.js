ead17c7f37a3720e049055c1a28ed077fb2a6fc1
Embbnux Ji <embbnux@embbnux.com>
2025-07-02 21:03:19 +0800

    feat(core): Add Jest coverage setup

--- packages/core/jest.config.js
+++ packages/core/jest.config.js
@@ -4,4 +4,14 @@ module.exports = {
   roots: ['<rootDir>/test'],
   testMatch: ['<rootDir>/test/**/*.test.ts'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/index.ts',
+    '!src/actionTypes.ts',
+    '!src/eventTypes.ts',
+    '!src/interfaces.ts',
+  ],
 };
